2c83b2f9938cd7a0db9530f4a00bad4bb86d8610
Nicole Dow <nikki.dow71@gmail.com>
2020-01-26 17:36:05 -0500

    reducer successfully updates state for adding the starting zipcode

--- client/src/reducers/handleInfo.js
+++ client/src/reducers/handleInfo.js
@@ -1,8 +1,18 @@
-const handleInfo = (state = { currentSection: 'Starting Zip Code' }, action) => {
+const handleInfo = (state = { 
+  currentSection: 'Starting Zip Code', 
+  formSections: ['Starting Zip Code', 'Destination Zip Code', 'Number of Bedrooms', 'How many floors?', 'Is there an elevator?', 'Where will the moving truck be able to park?', 'How can Millennial Moving Solutions Contact You?'],
+  currentIndex: 0
+}, action) => {
   switch(action.type){
   case 'ADD_CUSTOMER_INFO':
-    console.log('in reducer', action)
-    return {...state, data: action.formData}
+    switch(state.currentSection){
+      case 'Starting Zip Code':
+        let newCurrentIndex = state.currentIndex + 1;
+        let nextSection = state.formSections[newCurrentIndex];
+        return {...state, startingZipCode: action.formData.data, currentSection: nextSection, currentIndex: newCurrentIndex};
+      default:
+        return state;
+    }
   default:
     return state;
   }
